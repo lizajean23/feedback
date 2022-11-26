12b1f027bb2fa417d8ca980e1ada20dc85c5119d
User <lizi.guchua.1@btu.edu.ge>
2022-11-26 15:55:48 +0400

    first commit

--- app/src/main/java/com/example/feedback/MainActivity.kt
+++ app/src/main/java/com/example/feedback/MainActivity.kt
@@ -32,28 +32,37 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener{
 
-            if (yes.isChecked && name.text.isEmpty() || lastname.text.isEmpty() || feedback.text.isEmpty()) {
-                Toast.makeText(this, "გთხოვთ უპასუხეთ ორივე შეკითხვას", Toast.LENGTH_SHORT).show()
+            if(!yes.isChecked && !no.isChecked){
+                Toast.makeText(this, "მონიშნე კი ან არა", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            else if(no.isChecked && name.text.isEmpty() || lastname.text.isEmpty() || feedback.text.isEmpty()){
-                Toast.makeText(this, "გთხოვთ უპასუხეთ ორივე შეკითხვას", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
+            if(no.isChecked){
+                Toast.makeText(this, "თქვენ არ ხართ ჩვენი მომხმარებელი", Toast.LENGTH_SHORT).show()
+
             }
 
-            else if (name.length() < 3 || !name.text.any { it.isLetter() }){
-                name.error = "სახელი უნდა შეიცავდეს მინიმუმ 3 ციფრს "
-                return@setOnClickListener
+            else if (yes.isChecked) {
+
+                if (name.text.isEmpty() || lastname.text.isEmpty() || feedback.text.isEmpty()) {
+                    Toast.makeText(this, "გთხოვთ უპასუხეთ ორივე შეკითხვას", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+                
+                else if (name.length() < 3 || !name.text.any { it.isLetter() }){
+                    name.error = "სახელი უნდა შეიცავდეს მინიმუმ 3 ციფრს "
+                    return@setOnClickListener
+                }
+                else if (lastname.length() < 5 || !lastname.text.any { it.isLetter() }){
+                    lastname.error = "გვარი უნდა შეიცვადეს მინიმუმ 5 ციფრს"
+                    return@setOnClickListener
+                }
+                else if (feedback.length() < 10 || !lastname.text.any{it.isLetter()}){
+                    feedback.error = "უნდა შეიცავდეს მინიმუმ 10 სიმბოლოს"
+                    return@setOnClickListener
+                }
+                Toast.makeText(this, "გაგზავნილია", Toast.LENGTH_SHORT).show()
             }
-            else if (lastname.length() < 5 || !lastname.text.any { it.isLetter() }){
-                lastname.error = "გვარი უნდა შეიცვადეს მინიმუმ 5 ციფრს"
-                return@setOnClickListener
-            }
-            else if (feedback.length() < 10 || !lastname.text.any{it.isLetter()}){
-                feedback.error = "უნდა შეიცავდეს მინიმუმ 10 სიმბოლოს"
-                return@setOnClickListener
-            }
-            Toast.makeText(this, "გაგზავნილია", Toast.LENGTH_SHORT).show()
+
         }
 
     }
